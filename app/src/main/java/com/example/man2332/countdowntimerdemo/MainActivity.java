956c5e2535d748ca92e575fb0f3b4fbfa24bc7c7
john <poopman0202@gmail.com>
2019-01-17 08:28:20 -0500

    added shared preferences to save the timer when app is closed.

--- app/src/main/java/com/example/man2332/countdowntimerdemo/MainActivity.java
+++ app/src/main/java/com/example/man2332/countdowntimerdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.man2332.countdowntimerdemo;
 
+import android.content.SharedPreferences;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,10 +18,11 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonReset;
 
     private CountDownTimer mCountDownTimer;
-    //tells us if timer is running or not
-    private boolean mTimerRunning;//null
 
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis;
+    private long mEndTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +51,11 @@ public class MainActivity extends AppCompatActivity {
                 resetTimer();
             }
         });
-
-        updateCountDownText();
     }
 
-    //-startTimer() creates a new CountDownTimer and .start() it
     private void startTimer() {
+        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
+
         mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -65,42 +66,115 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 mTimerRunning = false;
-                mButtonStartPause.setText("Start");
-                mButtonStartPause.setVisibility(View.INVISIBLE);
-                mButtonReset.setVisibility(View.VISIBLE);
+                updateButtons();
             }
         }.start();
 
         mTimerRunning = true;
-        mButtonStartPause.setText("pause");
-        mButtonReset.setVisibility(View.INVISIBLE);
+        updateButtons();
     }
 
     private void pauseTimer() {
         mCountDownTimer.cancel();
         mTimerRunning = false;
-        mButtonStartPause.setText("Start");
-        mButtonReset.setVisibility(View.VISIBLE);
+        updateButtons();
     }
 
     private void resetTimer() {
         mTimeLeftInMillis = START_TIME_IN_MILLIS;
-        updateCountDownText();//set text back to orginal time
-        mButtonReset.setVisibility(View.INVISIBLE);
-        mButtonStartPause.setVisibility(View.VISIBLE);
+        updateCountDownText();
+        updateButtons();
     }
 
     private void updateCountDownText() {
         int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
         int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
-        //formats the string to 00:00 for the countdown text view
+
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
 
         mTextViewCountDown.setText(timeLeftFormatted);
     }
+
+    private void updateButtons() {
+        if (mTimerRunning) {
+            mButtonReset.setVisibility(View.INVISIBLE);
+            mButtonStartPause.setText("Pause");
+        } else {
+            mButtonStartPause.setText("Start");
+
+            if (mTimeLeftInMillis < 1000) {
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+            } else {
+                mButtonStartPause.setVisibility(View.VISIBLE);
+            }
+
+            if (mTimeLeftInMillis < START_TIME_IN_MILLIS) {
+                mButtonReset.setVisibility(View.VISIBLE);
+            } else {
+                mButtonReset.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putLong("millisLeft", mTimeLeftInMillis);
+        editor.putBoolean("timerRunning", mTimerRunning);
+        editor.putLong("endTime", mEndTime);
+
+        editor.apply();
+
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+
+        mTimeLeftInMillis = prefs.getLong("millisLeft", START_TIME_IN_MILLIS);
+        mTimerRunning = prefs.getBoolean("timerRunning", false);
+
+        updateCountDownText();
+        updateButtons();
+
+        if (mTimerRunning) {
+            mEndTime = prefs.getLong("endTime", 0);
+            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+
+            if (mTimeLeftInMillis < 0) {
+                mTimeLeftInMillis = 0;
+                mTimerRunning = false;
+                updateCountDownText();
+                updateButtons();
+            } else {
+                startTimer();
+            }
+        }
+    }
 }
 //creating a timer with import android.os.CountDownTimer;
+//-startTimer() creates a new CountDownTimer and .start() it
 //-countDownInterval is how often call the onTick method
 //     for example for us, every 1000milis(1 second) we will call onTick
+//  mEndTime is the future time when timer should end, it's current system time + time left
+//    if CountDownTimer isn't running, we already know the exact time the timer should stop with endTime
+//    countDownTimer might stop a bit when the screen rotates & activity gets destroyed
 //-public void onTick(long millisUntilFinished) {
 //  millisUntilFinished- is how many miliseconds until the countdown timer finishes(calls onFinish())
+//-updateCountDOwnText() - formats the string to 00:00 for the countdown text view
+//-onStop() - put in data in shared preferences(the time left & if timer is running)
+//  in this method we don't have a bundle to pass like onSaveInstanceState() for
+//  when the activity gets destroyed, but we use shared preferences to save data outside the lifetime
+//  of the app
+//-onStart() - take data from shared preferences(the time left & if timer is running)
+//-onSaveInstanceState() and onRestoreInstanceState() is used when the activity is destroyed but doesn't
+//  save data outside the life of the app, while shared preferences does
